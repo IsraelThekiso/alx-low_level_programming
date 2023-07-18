cc70721f717ebaca9bf8f1ffd448185a4c75124a
IsraelThekiso <thekisobaloyi@gmail.com>
2023-07-18 14:44:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * of an intiger.
+ *
+ * @i: input number as an intiger.
+ *
+ * Return: Absolute value
+ */
+int _abs(int i)
+{
+	if (i>=0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i *-1);
+	}
+}
