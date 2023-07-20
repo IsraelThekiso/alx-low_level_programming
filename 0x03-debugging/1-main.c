788b355ebb0378c173de1a90280fbcf52e8e2a5e
IsraelThekiso <thekisobaloyi@gmail.com>
2023-07-20 04:39:58 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - prevents an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
-    int i;
-    printf("Infinite loop incoming :(\n");
-    i = 0;
-    while (i < 10) // Correct the loop condition
-    {
-        putchar(i + '0'); // Print the digits 0 to 9
-        i++; // Increment i to avoid an infinite loop
-    }
-    printf("\nInfinite loop avoided! \\o/\n");
-    return (0);
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,35 +3,6 @@
 
 #include <stdio.h>
 
-/**
- * positive_or_negative - Checks if a number is positive, negative, or zero
- * @i: The number to be checked
- */
 void positive_or_negative(int i);
-
-/**
- * largest_number - Finds the largest among three numbers
- * @a: First number
- * @b: Second number
- * @c: Third number
- * Return: The largest number
- */
-int largest_number(int a, int b, int c);
-
-/**
- * print_remaining_days - Prints the number of days remaining in the year
- * @month: Month (1-12)
- * @day: Day (1-31)
- * @year: Year (full four-digit year)
- */
-void print_remaining_days(int month, int day, int year);
-
-/**
- * convert_day - Converts a given month and day to the day of the year
- * @month: Month (1-12)
- * @day: Day (1-31)
- * Return: The day of the year (1-366)
- */
-int convert_day(int month, int day);
 
 #endif /* MAIN_H */
