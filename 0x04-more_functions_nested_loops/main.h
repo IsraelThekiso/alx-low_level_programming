1cbf6fa3a2615b58ada9ceaca5d301b1d058449d
IsraelThekiso <thekisobaloyi@gmail.com>
2023-07-20 12:09:00 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,6 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -14,4 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
 #endif
