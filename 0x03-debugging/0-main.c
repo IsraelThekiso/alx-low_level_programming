c1ef90402f187292abc9851dd4a41fc75c44ebb7
IsraelThekiso <thekisobaloyi@gmail.com>
2023-07-20 04:29:57 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,17 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * positive_or_negative - Checks if a number is positive, negative, or zero
- * @n: The number to be checked
- */
-void positive_or_negative(int n)
-{
-    if (n > 0)
-        printf("%d is positive.\n", n);
-    else if (n < 0)
-        printf("%d is negative.\n", n);
-    else
-        printf("The number is zero.\n");
+void positive_or_negative(int num) {
+    if (num > 0) {
+        printf("%d is positive.\n", num);
+    } else if (num < 0) {
+        printf("%d is negative.\n", num);
+    } else {
+        printf("%d is zero.\n", num);
+    }
 }
 
-/**
- * main - Test function for positive_or_negative
- * Return: 0
- */
-int main(void)
-{
+int main(void) {
     int i = 0;
     positive_or_negative(i);
-    return (0);
+    return 0;
 }
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
-
+ * main - prevents an infinite loop
+ * Return: 0
+ */
 int main(void)
 {
-        int i;
-
-        printf("Infinite loop incoming :(\n");
-
-        i = 0;
-
-        while (i < 10)
-        {
-                putchar(i);
-        }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+    int i;
+    printf("Infinite loop incoming :(\n");
+    i = 0;
+    while (i < 10) // Correct the loop condition
+    {
+        putchar(i + '0'); // Print the digits 0 to 9
+        i++; // Increment i to avoid an infinite loop
+    }
+    printf("\nInfinite loop avoided! \\o/\n");
+    return (0);
 }
